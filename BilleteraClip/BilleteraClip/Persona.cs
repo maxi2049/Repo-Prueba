72db7053112203fcbc795bd82d4428e24fc81443
Maximiliano Iglesias <maxi2049@hotmail.com>
2020-11-16 00:30:57 -0300

    Pruebas de C#

---
+++ BilleteraClip/BilleteraClip/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilleteraClip
+{
+    class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public Persona(string nombre, string apellido)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+
+        public Persona()
+        {
+
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+        public override string ToString()
+        {
+            return apellido + ", " + nombre;
+        }
+    }
+}
---
+++ BilleteraClip/BilleteraClip/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BilleteraClip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona objetoPersona = new Persona("Maximo", "Iglesias");
+            objetoPersona.Apellido = "Rojas";
+            Console.WriteLine(objetoPersona.ToString());
+
+            Persona objetoPersona2 = new Persona();
+            objetoPersona2.Apellido = "Iglesias";
+            objetoPersona2.Nombre = "Maximiliano";
+            Console.WriteLine(objetoPersona2.ToString());
+        }
+    }
+}
